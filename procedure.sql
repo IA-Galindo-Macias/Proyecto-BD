b8a6d78c808cdb9340a3f30e3ebf29d32e65ebc2
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-11-23 00:06:32 -0800

    procedures documentados

--- procedure.sql
+++ procedure.sql
@@ -22,9 +22,15 @@ BEGIN
 END 
 %%
 
--- -----------------------------------------------------
--- Crear integrantes : Luis Eduardo Galindo Amaya
--- -----------------------------------------------------
+/**
+ * @autor Luis Eduardo Galindo Amaya
+ * Crea un nuevo integrante en el equipo.
+ * 
+ * @param p_nombre      Nombre del nuevo integrante.
+ * @param p_email       Email del nuevo integrante.
+ * @param p_salario     Salario del nuevo integrante.
+ * @param p_equipo_id   ID del equipo al que pertenece.
+ */
 DELIMITER %%
 CREATE PROCEDURE crear_integrante(
 	IN p_nombre VARCHAR(45),
@@ -49,9 +55,12 @@ END
 %%
 
 
--- -----------------------------------------------------
--- Crear equipos : Luis Eduardo Galindo Amaya
--- -----------------------------------------------------
+/**
+ * @autor Luis Eduardo Galindo Amaya
+ * Crea un nuevo equipo.
+ *
+ * @param p_nombre Nombre del nuevo equipo.
+ */
 DELIMITER %%
 CREATE PROCEDURE crear_equipo(
 	IN p_nombre VARCHAR(45)
@@ -69,9 +78,15 @@ END
 %%
 
 
--- -----------------------------------------------------
--- Crear tarea : Luis Eduardo Galindo Amaya 
--- -----------------------------------------------------
+/**
+ * @autor Luis Eduardo Galindo Amaya
+ * Crea una nueva tarea.
+ *
+ * @param p_inicio       Fecha de inicio de la tarea.
+ * @param p_nombre       Nombre de la tarea.
+ * @param p_descripcion  Descripción de la tarea.
+ * @param p_duracion     Duración estimada de la tarea.
+ */
 DELIMITER %%
 CREATE PROCEDURE crear_tarea(
     IN p_inicio DATETIME,
@@ -98,9 +113,13 @@ END
 %%
 
 
--- -----------------------------------------------------
--- asignar equipo tarea : Luis Eduardo Galindo Amaya 
--- -----------------------------------------------------
+/**
+ * @autor Luis Eduardo Galindo Amaya
+ * Asigna un equipo a una tarea existente.
+ *
+ * @param p_id_equipo ID del equipo a asignar.
+ * @param p_id_tarea  ID de la tarea a la que se asignará el equipo.
+ */
 DELIMITER %%
 CREATE PROCEDURE asignar_equipo_tarea(
     IN p_id_equipo INT,
@@ -114,9 +133,14 @@ END
 %%
 
 
--- -----------------------------------------------------
--- asignar integrante tarea : Luis Eduardo Galindo Amaya 
--- -----------------------------------------------------
+/**
+ * @autor Luis Eduardo Galindo Amaya
+ * Asigna un integrante a una tarea existente.
+ *
+ * @param p_id_integrante ID del integrante.
+ * @param p_id_tarea      ID de la tarea a la que se asignará.
+ * @return Mensaje indicando si la asignación fue exitosa o no.
+ */
 DELIMITER %%    
 CREATE PROCEDURE asignar_integrante_tarea(
     IN p_id_integrante INT,
