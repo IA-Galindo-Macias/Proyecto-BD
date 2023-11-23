0b4f6033a320dd9ce57ec3bbc4505a4570a2087b
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-11-22 18:49:52 -0800

    crear equipos actualizado

--- procedure.sql
+++ procedure.sql
@@ -1,11 +1,51 @@
 use mydb;
 
+
+-- -----------------------------------------------------
+-- Crear equipos : Luis Eduardo Galindo Amaya
+-- -----------------------------------------------------
 DELIMITER %%
-CREATE PROCEDURE crear_equipo(IN nombre_equipo VARCHAR(45))
+CREATE PROCEDURE crear_equipo(IN p_nombre VARCHAR(45))
 BEGIN
-	INSERT INTO equipo (`equipo_nombre`) VALUES (nombre_equipo);
+	INSERT INTO equipo (`equipo_nombre`, `equipo_estatus`) 
+    VALUES (p_nombre, "activo");
 END
 %%
 
-call crear_equipo("Hola");
 
+
+
+-- -----------------------------------------------------
+-- Crear tarea : Luis Eduardo Galindo Amaya 
+-- -----------------------------------------------------
+DELIMITER %%
+CREATE PROCEDURE insertar_tarea(
+    IN p_inicio DATETIME,
+    IN p_nombre VARCHAR(45),
+    IN p_descripcion VARCHAR(255),
+    IN p_duracion INT,
+    IN p_status INT,
+    IN p_integrante_id INT,
+    IN p_equipo_id INT
+)
+BEGIN
+    INSERT INTO tarea (
+        tarea_inicio,
+        tarea_nombre,
+        tarea_descripcion,
+        tarea_duracion,
+        tarea_status,
+        integrante_id_integrante,
+        equipo_id_equipo
+    )
+    VALUES (
+        p_inicio,
+        p_nombre,
+        p_descripcion,
+        p_duracion,
+        p_status,
+        p_integrante_id,
+        p_equipo_id
+    );
+END
+%%
--- tests.sql
+++ tests.sql
@@ -1,42 +1,24 @@
--- Inserción de Datos
+-- Inserciones para la tabla `equipo`
+INSERT INTO `mydb`.`equipo` (`equipo_nombre`, `equipo_estatus`) VALUES ('Equipo1', 'activo');
 
--- Inserción de un Equipo
-INSERT INTO `mydb`.`equipo` (`id_equipo`, `equipo_nombre`) VALUES (1, 'Equipo A');
+-- Inserciones para la tabla `integrante`
+INSERT INTO `mydb`.`integrante` (`integrante_nombre`, `integrantecol_email`, `equipo_id_equipo`) VALUES ('Juan Pérez', 'juan@example.com', equipo_id_equipo);
+INSERT INTO `mydb`.`integrante` (`integrante_nombre`, `integrantecol_email`, `equipo_id_equipo`) VALUES ('María Rodríguez', 'maria@example.com', equipo_id_equipo_cancelado);
 
--- Inserción de un Integrante en un Equipo
-INSERT INTO `mydb`.`integrante` (`integrante_nombre`, `integrantecol_email`, `equipo_id_equipo`) VALUES ('Juan Perez', 'juan@example.com', 1);
+-- Inserciones para la tabla `historial_activo`
+INSERT INTO `mydb`.`historial_activo` (`historial_activo_anterior`, `historial_activo_siguiente`, `id_integrante`) VALUES (1, 0, id_integrante);
 
--- Inserción de un Historial Activo para un Integrante
-INSERT INTO `mydb`.`historial_activo` (`historial_activo_anterior`, `historial_activo_siguiente`, `id_integrante`) VALUES (0, 1, 1);
+-- Inserciones para la tabla `historial_salario`
+INSERT INTO `mydb`.`historial_salario` (`historial_salario_anterior`, `historial_salario_siguiente`, `id_integrante`) VALUES (50000, 55000, id_integrante);
 
--- Inserción de un Historial Salario para un Integrante
-INSERT INTO `mydb`.`historial_salario` (`historial_salario_anterior`, `historial_salario_siguiente`, `id_integrante`) VALUES (50000, 60000, 1);
+-- Inserciones para la tabla `control_horario`
+INSERT INTO `mydb`.`control_horario` (`control_horario_salida`, `id_integrante`) VALUES (NULL, id_integrante);
 
--- Inserción de un Control Horario para un Integrante
-INSERT INTO `mydb`.`control_horario` (`control_horario_entrada`, `id_integrante`) VALUES ('2023-11-22 08:00:00', 1);
+-- Inserciones para la tabla `tarea`
+INSERT INTO `mydb`.`tarea` (`tarea_inicio`, `tarea_nombre`, `tarea_descripcion`, `tarea_duracion`, `tarea_status`, `integrante_id_integrante`, `equipo_id_equipo`) VALUES ('2023-11-22 12:00:00', 'Tarea1', 'Descripción de Tarea1', 5, 'pendiente', integrante_id_integrante, equipo_id_equipo);
 
--- Inserción de una Tarea asignada a un Integrante y Equipo
-INSERT INTO `mydb`.`tarea` (`id_tarea`, `tarea_inicio`, `tarea_nombre`, `tarea_descripcion`, `tarea_duracion`, `tarea_status`, `integrante_id_integrante`, `equipo_id_equipo`) VALUES (1, '2023-11-22 09:00:00', 'Tarea 1', 'Realizar tarea 1', 4, 0, 1, 1);
+-- Inserciones para la tabla `historal_prorroga`
+INSERT INTO `mydb`.`historal_prorroga` (`historal_prorroga_duracion`, `tarea_id_tarea`) VALUES (2, tarea_id_tarea);
 
--- Inserción de un Historial de Prórroga para una Tarea
-INSERT INTO `mydb`.`historal_prorroga` (`tarea_id_tarea`, `historal_prorroga_duracion`) VALUES (1, 2);
-
--- Inserción de una Tarea Completada
-INSERT INTO `mydb`.`tarea_completada` (`tarea_completada_marca`, `tarea_id_tarea`) VALUES ('2023-11-22 15:00:00', 1);
-
--- Consultas
-
--- Obtener todos los Integrantes de un Equipo
-SELECT * FROM `mydb`.`integrante` WHERE `equipo_id_equipo` = 1;
-
--- Obtener el Historial Activo de un Integrante
-SELECT * FROM `mydb`.`historial_activo` WHERE `id_integrante` = 1;
-
--- Obtener el Salario Actual de un Integrante
-SELECT `historial_salario_siguiente` FROM `mydb`.`historial_salario` WHERE `id_integrante` = 1 ORDER BY `historial_salario_marca` DESC LIMIT 1;
-
--- Obtener las Tareas asignadas a un Equipo
-SELECT * FROM `mydb`.`tarea` WHERE `equipo_id_equipo` = 1;
-
--- Obtener las Tareas Completadas
-SELECT * FROM `mydb`.`tarea_completada`;
+-- Inserciones para la tabla `tarea_completada`
+INSERT INTO `mydb`.`tarea_completada` (`tarea_id_tarea`) VALUES (tarea_id_tarea);
